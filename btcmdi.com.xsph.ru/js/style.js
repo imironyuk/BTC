43299de31e262147ac03e6587057060eb4acd57d
Daniil <imironyuk1979@gmail.com>
2021-04-30 18:06:50 +0300

    0.5

--- btcmdi.com.xsph.ru/js/style.js
+++ btcmdi.com.xsph.ru/js/style.js
@@ -1,44 +1,34 @@
-	// Селектор кнопки 
+// Селектор кнопки 
+const btn = document.querySelector("#switchMode");
 
-	const btn = document.querySelector("#switchMode");
-
-	// Селектор таблицы стилей <link>
-
-	const theme = document.querySelector("#theme");
-	
+// Селектор таблицы стилей <link>
+const theme = document.querySelector("#theme");
+// Хранение переменной "dark.css" для нового пользователя
 if (!localStorage.theme) localStorage.theme = "css/dark.css"
 theme.href = localStorage.theme
-
-	
-
 	// Клик по кнопке
 
 	document.getElementById("switchMode").onclick = function () {
 
-		// Если текущий урл содержит "light.css"
+		// Если текущий урл содержит "dark.css"
 
 		if (theme.getAttribute("href") == "css/dark.css") {
 
-			// тогда переключим его на "dark.css"
-
+			// тогда переключим его на "light.css"
 			theme.href = "css/light.css";
 
 				var theme_cookie = "css/light.css";
 
-			// Иначе
-
+		// Иначе
 		} else {
 
-			// переключаем его на "light.css"
-
+			// переключаем его на "dark.css"
 			theme.href = "css/dark.css";
 
 				var theme_cookie = "css/dark.css";
 		}
 
-			
 		// Пример использования:
-		localStorage.setItem('theme',theme_cookie);
-			
+		localStorage.setItem('theme', theme_cookie);
 
 	};
