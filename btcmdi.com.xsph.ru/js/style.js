8c3e6d6737c050baa87251933bfd78e3f256954e
Даниил <34751397+imironyuk@users.noreply.github.com>
2021-05-01 20:48:19 +0300

    Update style.js

--- btcmdi.com.xsph.ru/js/style.js
+++ btcmdi.com.xsph.ru/js/style.js
@@ -3,11 +3,13 @@ const btn = document.querySelector("#switchMode");
 
 // Селектор таблицы стилей <link>
 const theme = document.querySelector("#theme");
+
 // Хранение переменной "dark.css" для нового пользователя
 if (!localStorage.theme) localStorage.theme = "css/dark.css"
+// сменим "herf" на значение сохраненного значени е переменной в "localStorage.theme"
 theme.href = localStorage.theme
-	// Клик по кнопке
 
+	// Клик по кнопке
 	document.getElementById("switchMode").onclick = function () {
 
 		// Если текущий урл содержит "dark.css"
@@ -17,6 +19,7 @@ theme.href = localStorage.theme
 			// тогда переключим его на "light.css"
 			theme.href = "css/light.css";
 
+                               // сохраним значение стиля в переменную "theme_cookie"
 				var theme_cookie = "css/light.css";
 
 		// Иначе
@@ -25,6 +28,7 @@ theme.href = localStorage.theme
 			// переключаем его на "dark.css"
 			theme.href = "css/dark.css";
 
+                                // сохраним значение стиля в переменну "theme_cookie"
 				var theme_cookie = "css/dark.css";
 		}
 
